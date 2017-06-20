8ec30298015c9cd9af93b93b19a608bd097ea202
BuildTools <unconfigured@null.spigotmc.org>
2017-06-20 22:28:29 +0100

    Map Added

--- website.py
+++ website.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/map/')
+def map():
+    return render_template("map.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
